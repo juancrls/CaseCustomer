10c20c134158a6b6c02165fdfe709f46127b39ba
juancrls <juan1112@outlook.com.br>
2023-10-10 19:46:17 -0300

    feat: Add IoC project layer

---
+++ IoC/Unity/DependencyResolver.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Domain.Interfaces.Repositories;
+using Domain.Interfaces.Services;
+using Domain.Services;
+using Infrastructure.Persist.AutoMapper;
+using Infrastructure.Persist.Repositories;
+using Unity;
+using Unity.Lifetime;
+
+namespace IoC.Unity
+{
+    public static class DependencyResolver
+    {
+        public static void Resolve(UnityContainer container)
+        {
+            Configuration(container);
+            Services(container);
+            Repositories(container);
+        }
+
+        public static void Configuration(UnityContainer container)
+        {
+            var mapper = AutoMapperConfig.Configure();
+            container.RegisterInstance<IMapper>(mapper);
+        }
+
+
+        public static void Services(UnityContainer container)
+        {
+            container.RegisterType<ICustomerService, CustomerService>(new HierarchicalLifetimeManager());
+        }
+
+        public static void Repositories(UnityContainer container)
+        {
+            container.RegisterType(typeof(IBaseRepository<,>), typeof(BaseRepository<,>));
+            container.RegisterType<ICustomerRepository, CustomerRepository>(new HierarchicalLifetimeManager());
+        }
+    }
+}
---
+++ IoC/Unity/UnityResolver.cs
@@ -0,0 +1,21 @@
+﻿using Unity;
+
+public class UnityResolver
+{
+    private readonly IUnityContainer _container;
+
+    public UnityResolver(IUnityContainer container)
+    {
+        _container = container;
+    }
+
+    public T Resolve<T>()
+    {
+        return _container.Resolve<T>();
+    }
+
+    public void RegisterType<TFrom, TTo>() where TTo : TFrom
+    {
+        _container.RegisterType<TFrom, TTo>();
+    }
+}
